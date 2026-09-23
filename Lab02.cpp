e3a267e17096768c3282054481080b61a06ce4d1
Andriy <andrii.kovmir.ri.2026@lpnu.ua>
2026-09-23 17:31:02 +0300

    first commit

---
+++ Lab02.cpp
@@ -0,0 +1,29 @@
+// Lab02.cpp
+// Ковмір Андрій
+// Лабораторна робота №2
+// Лінійні програми
+// Варіант 14
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double alpha; // вхідний параметр(радіани)
+	double z1; // результат 1-го виразу
+	double z2; // результат 2-го виразу
+
+	cout << "alpha = "; cin >> alpha; // введення з клавіатури
+
+	z1 = (cos(alpha) + sin(alpha)) / (cos(alpha) - sin(alpha));
+	z2 = tan(2*alpha) + 1.0 / cos(2*alpha); // sec(2*alpha) = 1/cos(2*alpha)
+
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+
+	cin.get();//зупиняє консоль
+	return 0;
+}
