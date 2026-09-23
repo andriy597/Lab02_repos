5c43024c038d72c4f3032b2f8c1686ba3cfc3757
Andriy <andrii.kovmir.ri.2026@lpnu.ua>
2026-09-23 18:53:29 +0300

    second commit in master - added extra line

--- Lab02.cpp
+++ Lab02.cpp
@@ -24,6 +24,7 @@ int main()
 	cout << "z1 = " << z1 << endl;
 	cout << "z2 = " << z2 << endl;
 	cout << "Програма завершена" << endl;
+	cout << "This is master branch version" << endl;
 
 	cin.get();//зупиняє консоль
 	return 0;
